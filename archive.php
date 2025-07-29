a9032dc89a056d64f6c2faab04eb231b5721d491
Chaithra K <chaithravalanja@gmail.com>
2025-07-29 17:43:53 +0530

    Initial commit of InspirePress theme

--- archive.php
+++ archive.php
@@ -6,7 +6,12 @@
 
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
-                <article <?php post_class(); ?>>
+                <article <?php post_class('animated-blob-card'); ?>>
+                    <div class="blob-thumbnail">
+                        <?php if (has_post_thumbnail()) : ?>
+                            <?php the_post_thumbnail('medium'); ?>
+                        <?php endif; ?>
+                    </div>
                     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <?php the_excerpt(); ?>
                 </article>
